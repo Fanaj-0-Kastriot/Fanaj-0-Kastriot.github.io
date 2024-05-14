2b563ad44485eff0a2317faacb9391d1ed2091f3
Fanaj-0-Kastriot <152373052+Fanaj-0-Kastriot@users.noreply.github.com>
2024-05-15 00:21:46 +0200

    Delete javascript directory

--- javascript/app.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("registrationForm").addEventListener("submit", function(event) {
-    event.preventDefault();
-
-    var email = document.getElementById("email").value;
-    var password = document.getElementById("password").value;
-
-    alert("Inscription réussie !");
-});
